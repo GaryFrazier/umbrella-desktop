71d8604ac0ce2e6b6ba58a72640c0e38babd3ef6
frazier_g <garyfrazier95@yahoo.com>
2024-05-20 14:09:06 -0400

    it spins

--- src/app/components/startupLogo.js
+++ src/app/components/startupLogo.js
@@ -1,24 +1,47 @@
 'use client';
 import { useEffect, useRef } from "react";
+import UmbrellaTriangleSvg from '../svg/umbrellaTriangle';
 import styles from "./startupLogo.module.css";
 
-async function startAnimation(logoAudio) {
-  setTimeout(() => {
+async function startSound(logoAudio) {
     if (logoAudio.current) {
         logoAudio.current.play();
     }
-  }, 500);
 }
 
 export default function StartupLogo() {
   const logoAudio = useRef(null);
 
   useEffect(() => {
-    startAnimation(logoAudio);
+    startSound(logoAudio);
   }, [])
 
   return (
     <div className={styles.startupLogo}>
+        <div className={styles.tri1}>
+            <UmbrellaTriangleSvg  />
+        </div>
+        <div className={styles.tri2}>
+            <UmbrellaTriangleSvg  />
+        </div>
+        <div className={styles.tri3}>
+            <UmbrellaTriangleSvg  />
+        </div>
+        <div className={styles.tri4}>
+            <UmbrellaTriangleSvg  />
+        </div>
+        <div className={styles.tri5}>
+            <UmbrellaTriangleSvg  />
+        </div>
+        <div className={styles.tri6}>
+            <UmbrellaTriangleSvg  />
+        </div>
+        <div className={styles.tri7}>
+            <UmbrellaTriangleSvg  />
+        </div>
+        <div className={styles.tri8}>
+            <UmbrellaTriangleSvg  />
+        </div>
         <audio ref={logoAudio} src="/startup_logo.mp3" />
     </div>
   );
---
+++ src/app/svg/umbrellaTriangle.js
@@ -0,0 +1,7 @@
+const UmbrellaTriangleSvg = () => (
+  <svg width="200" height="200" viewBox="0 0 200 200">
+    <path d="M100,10 L30,180 Q100,160 170,180 Z" fill="#cd0029" />
+  </svg>
+);
+
+export default UmbrellaTriangleSvg;
