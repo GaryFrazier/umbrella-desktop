3388bf4f60a79b70cf807592578ad6cc579d578c
frazier_g <garyfrazier95@yahoo.com>
2024-05-19 20:09:29 -0400

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,35 @@
 'use client';
-
-import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
-async function setupAppWindow() {
+async function setupAppWindow(startupAudio, logoAudio) {
   const appWindow = (await import('@tauri-apps/api/window')).appWindow;
 
   setTimeout(() => {
     appWindow.show();
+    if (startupAudio.current) {
+      startupAudio.current.addEventListener('ended', () => {
+        if (logoAudio.current) {
+          logoAudio.current.play();
+        }
+      });
+      startupAudio.current.play();
+    }
   }, 500);
 }
 
 export default function Home() {
+  const startupAudio = useRef(null);
+  const logoAudio = useRef(null);
+
   useEffect(() => {
-    setupAppWindow()
+    setupAppWindow(startupAudio, logoAudio);
   }, [])
 
   return (
     <main className={styles.main}>
-
+        <audio ref={startupAudio} src="/startup.mp3" />
+        <audio ref={logoAudio} src="/startup_logo.mp3" />
     </main>
   );
 }
