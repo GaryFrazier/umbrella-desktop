e7944e6021f3bac3514aea59453b1290226ae48a
frazier_g <garyfrazier95@yahoo.com>
2024-05-21 12:19:45 -0400

    transitionss

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
 import StartupLogo from "./components/startupLogo";
+import Image from 'next/image';
+import Ropls from '../public/ropls.png';
 
-async function setupAppWindow(startupAudio, setShowLogo) {
+async function setupAppWindow(startupAudio, setShowLogo, setShowRopls) {
   const appWindow = (await import('@tauri-apps/api/window')).appWindow;
 
   setTimeout(() => {
@@ -11,6 +13,10 @@ async function setupAppWindow(startupAudio, setShowLogo) {
     if (startupAudio.current) {
       startupAudio.current.addEventListener('ended', () => {
         setShowLogo(true);
+        
+        setTimeout(() => {
+          setShowRopls(true);
+        }, 9750);
       });
       startupAudio.current.play();
     }
@@ -20,14 +26,19 @@ async function setupAppWindow(startupAudio, setShowLogo) {
 export default function Home() {
   const startupAudio = useRef(null);
   const [showLogo, setShowLogo] = useState(false);
+  const [showRopls, setShowRopls] = useState(false);
 
   useEffect(() => {
-    setupAppWindow(startupAudio, setShowLogo);
+    setupAppWindow(startupAudio, setShowLogo, setShowRopls);
   }, [])
 
   return (
     <main className={styles.main}>
         <audio ref={startupAudio} src="/startup.mp3" />
+        {showRopls ? <Image
+          src={Ropls}
+          className={styles.roplsLogo}
+        /> : null}
         {showLogo ? <StartupLogo /> : null }
     </main>
   );
