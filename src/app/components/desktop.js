bd73f7ea5f0eb4bdf9f1d69193bdc62dc94c39b2
frazier_g <garyfrazier95@yahoo.com>
2024-05-21 12:36:36 -0400

    icons

---
+++ src/app/components/desktop.js
@@ -0,0 +1,11 @@
+'use client';
+import styles from "./desktop.module.css";
+
+
+export default function Desktop() {
+  return (
+    <div className={styles.desktop}>
+
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,11 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
 import StartupLogo from "./components/startupLogo";
+import Desktop from "./components/desktop";
 import Image from 'next/image';
 import Ropls from '../public/ropls.png';
 
-async function setupAppWindow(startupAudio, setShowLogo, setShowRopls) {
+async function setupAppWindow(startupAudio, setShowLogo, setShowRopls, setShowDesktop) {
   const appWindow = (await import('@tauri-apps/api/window')).appWindow;
 
   setTimeout(() => {
@@ -16,6 +17,12 @@ async function setupAppWindow(startupAudio, setShowLogo, setShowRopls) {
         
         setTimeout(() => {
           setShowRopls(true);
+
+          setTimeout(() => {
+            setShowLogo(false);
+            setShowRopls(false);
+            setShowDesktop(true);
+          }, 2000)
         }, 9750);
       });
       startupAudio.current.play();
@@ -27,14 +34,18 @@ export default function Home() {
   const startupAudio = useRef(null);
   const [showLogo, setShowLogo] = useState(false);
   const [showRopls, setShowRopls] = useState(false);
+  const [showDesktop, setShowDesktop] = useState(false);
 
   useEffect(() => {
-    setupAppWindow(startupAudio, setShowLogo, setShowRopls);
+    setupAppWindow(startupAudio, setShowLogo, setShowRopls, setShowDesktop);
   }, [])
 
   return (
     <main className={styles.main}>
         <audio ref={startupAudio} src="/startup.mp3" />
+        {showDesktop ? 
+          <Desktop />
+        : null}
         {showRopls ? <Image
           src={Ropls}
           className={styles.roplsLogo}
